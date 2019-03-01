ad18c2172ba3e373d9a86ea0cc0787f16950cb60
lijinbo <lilinghao1986@126.com>
2019-03-01 09:25:11 +0800

    添加BaseEntityRepository

---
+++ src/Galaxy.Libra.DapperExtensions/EntityRepository/BaseEntityRepository.cs
@@ -0,0 +1,123 @@
+﻿using Dapper;
+using Galaxy.Libra.DapperExtensions.Predicate;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq.Expressions;
+
+namespace Galaxy.Libra.DapperExtensions.EntityRepository
+{
+    public abstract class BaseEntityRepository<T> : IBaseEntityRepository<T> where T : class
+    {
+        protected IDbConnection curDbConnection = null;
+
+        #region 增删改
+
+        public virtual dynamic Add(T entity)
+        {
+            return Execute(() => curDbConnection.Insert<T>(entity));
+        }
+
+        public virtual bool Update(T entity)
+        {
+            return Execute(() => curDbConnection.Update(entity));
+        }
+
+        public virtual bool Delete(Expression<Func<T, bool>> expression)
+        {
+            return Execute(() => curDbConnection.Delete(expression));
+        }
+
+        #endregion
+
+        /// <summary>
+        /// 获取,参数为null时，则获取整个表数据
+        /// </summary>
+        /// <param name="predicate">参数示例，如：new { LastName = "Bar", FirstName = "Foo" }</param>
+        public virtual List<T> GetList(object predicate = null)
+        {
+            return Execute(() => curDbConnection.GetList<T>(predicate).AsList<T>());
+        }
+
+        public virtual List<T> GetList(Expression<Func<T, bool>> expression)
+        {
+            return Execute(() => curDbConnection.GetList<T>(expression).AsList<T>());
+        }
+
+        /// <summary>
+        /// 获取分页数据
+        /// </summary>
+        /// <param name="page">页索引</param>
+        /// <param name="resultsPerPage">每页记录数</param>
+        /// <param name="predicate">参数示例，如：new { LastName = "Bar", FirstName = "Foo" }, 更多的请使用Predicates</param>
+        public virtual List<T> GetPage(int page, int resultsPerPage, IList<ISort> sort, object predicate = null)
+        {
+            return Execute(() => curDbConnection.GetPage<T>(predicate, sort, page, resultsPerPage).AsList<T>());
+        }
+
+        /// <summary>
+        /// 获取分页数据
+        /// </summary>
+        /// <param name="page">页索引</param>
+        /// <param name="resultsPerPage">每页记录数</param>
+        public virtual List<T> GetPage(int page, int resultsPerPage, IList<ISort> sort, Expression<Func<T, bool>> expression)
+        {
+            return Execute(() => curDbConnection.GetPage<T>(expression, sort, page, resultsPerPage).AsList<T>());
+        }
+
+        /// <summary>
+        /// 数量
+        /// </summary>
+        /// <param name="predicate">参数示例，如：new { LastName = "Bar", FirstName = "Foo" }</param>
+        /// <returns></returns>
+        public virtual int Count(object predicate = null)
+        {
+            return Execute(() => curDbConnection.Count<T>(predicate));
+        }
+
+        /// <summary>
+        /// 数量
+        /// </summary>
+        /// <param name="predicate">参数示例，如：new { LastName = "Bar", FirstName = "Foo" }</param>
+        /// <returns></returns>
+        public virtual int Count(Expression<Func<T, bool>> expression)
+        {
+            return Execute(() => curDbConnection.Count<T>(expression));
+        }
+
+        private R Execute<R>(Func<R> fun)
+        {
+            if (curDbConnection != null && fun != null)
+            {
+                using (curDbConnection)
+                {
+                    if (curDbConnection.State != ConnectionState.Open)
+                        curDbConnection.Open();
+
+                    R res = fun();
+
+                    curDbConnection.Close();
+                    return res;
+                }
+            }
+
+            return default(R);
+        }
+
+        private void ExecuteNoValue(Action action)
+        {
+            if (curDbConnection != null && action != null)
+            {
+                using (curDbConnection)
+                {
+                    if (curDbConnection.State != ConnectionState.Open)
+                        curDbConnection.Open();
+
+                    action();
+
+                    curDbConnection.Close();
+                }
+            }
+        }
+    }
+}
---
+++ src/Galaxy.Libra.DapperExtensions/EntityRepository/IBaseEntityRepository.cs
@@ -0,0 +1,50 @@
+﻿using Galaxy.Libra.DapperExtensions.Predicate;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace Galaxy.Libra.DapperExtensions.EntityRepository
+{
+    public interface IBaseEntityRepository<T> where T : class
+    {
+        #region 增删改
+
+        dynamic Add(T entity);
+
+        bool Update(T entity);
+
+        bool Delete(Expression<Func<T, bool>> expression);
+
+        #endregion
+
+        /// <summary>
+        /// 获得,全部参数为null
+        /// </summary>
+        /// <param name="predicate">参数示例，如：new { LastName = "Bar", FirstName = "Foo" }, 更多的请使用Predicates</param>
+        /// <returns></returns>
+        List<T> GetList(object predicate = null);
+
+        List<T> GetList(Expression<Func<T, bool>> expression);
+
+        /// <summary>
+        /// 获取分页数据
+        /// </summary>
+        /// <param name="page">页索引</param>
+        /// <param name="resultsPerPage">每页记录数</param>
+        /// <param name="buffered">是否缓存</param>
+        /// <param name="predicate">参数示例，如：new { LastName = "Bar", FirstName = "Foo" }, 更多的请使用Predicates</param>
+        /// <returns></returns>
+        List<T> GetPage(int page, int resultsPerPage, IList<ISort> sort, object predicate = null);
+
+        List<T> GetPage(int page, int resultsPerPage, IList<ISort> sort, Expression<Func<T, bool>> expression);
+
+        /// <summary>
+        /// 数量
+        /// </summary>
+        /// <param name="predicate">参数示例，如：new { LastName = "Bar", FirstName = "Foo" }, 更多的请使用Predicates</param>
+        /// <returns></returns>
+        int Count(object predicate = null);
+
+        int Count(Expression<Func<T, bool>> expression);
+    }
+}
